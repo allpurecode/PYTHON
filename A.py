75651094258b36a5bd7b51407092a413d0ea623d
allpurecode <rishika212110@gmail.com>
2024-09-09 10:33:50 +0530

    09/09/2024

--- A.py
+++ A.py
@@ -139,7 +139,7 @@ res = reduce(perform, data)
 print(res)'''
 
 
-def antique(result, num):
+"""def antique(result, num):
     if num % 2 == 0:
         return result + (2 * num)
     else:
@@ -148,4 +148,81 @@ def antique(result, num):
 
 num = [1, 2, 3]
 res = reduce(antique, num)
-print(res)
+print(res)"""
+#----------------------------------------------------------------------------------------------------------------------------------------
+
+# Python3 program to show that the variables with a value
+# assigned in the class declaration, are class variables and
+# variables inside methods and constructors are instance
+# variables.
+
+# Class for Dog
+
+
+"""class Dog:
+
+    # Class Variable
+    animal = 'dog'
+
+    # The init method or constructor
+    def __init__(self, breed, color):
+
+        # Instance Variable
+        self.breed = breed
+        self.color = color
+
+
+# Objects of Dog class
+Rodger = Dog("Pug", "brown")
+Buzo = Dog("Bulldog", "black")
+
+print('Rodger details:')
+print('Rodger is a', Rodger.animal)
+print('Breed: ', Rodger.breed)
+print('Color: ', Rodger.color)
+
+print('\nBuzo details:')
+print('Buzo is a', Buzo.animal)
+print('Breed: ', Buzo.breed)
+print('Color: ', Buzo.color)
+
+# Class variables can be accessed using class
+# name also
+print("\nAccessing class variable using class name")
+print(Dog.animal)"""
+
+
+
+# Python3 program to show that we can create
+# instance variables inside methods
+
+# Class for Dog
+
+
+"""class Dog:
+
+    # Class Variable
+    animal = 'dog'
+
+    # The init method or constructor
+    def __init__(self, breed):
+
+        # Instance Variable
+        self.breed = breed
+
+    # Adds an instance variable
+    def setColor(self, color):
+        self.color = color
+
+    # Retrieves instance variable
+    def getColor(self):
+        return self.color
+
+
+# Driver Code
+Rodger = Dog("pug")
+Rodger.setColor("brown")
+print(Rodger.getColor())"""
+
+
+
