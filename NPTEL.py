a8bf4444196d5d9e4b45cda7731806e7de3ea70c
allpurecode <rishika212110@gmail.com>
2024-09-23 10:44:38 +0530

    23/09/2024

--- NPTEL.py
+++ NPTEL.py
@@ -1,53 +1,61 @@
 def intreverse(n):
-    revno=0
-    while n!=0:
-        r=n%10
-        revno=revno*10 + r
-        n=n//10
+    revno = 0
+    while n != 0:
+        r = n % 10
+        revno = revno * 10 + r
+        n = n // 10
     return revno
+
+
 def matched(n):
-    n=list(n)
-    c=0
+    n = list(n)
+    c = 0
     for i in range(len(n)):
-        if c==-1:
+        if c == -1:
             return False
-        if n[i]=="(":
-            c=c+1
-        if n[i]==")":
-            c=c-1
-    if c==0:
+        if n[i] == "(":
+            c = c + 1
+        if n[i] == ")":
+            c = c - 1
+    if c == 0:
         return True
     else:
         return False
 
+
 def sumprimes(l):
-    s=0
+    s = 0
     for i in l:
-        if i <1:
-            s=s+0
+        if i < 1:
+            s = s + 0
         else:
-            for j in range(2,i):
-                if i % j==0:
+            for j in range(2, i):
+                if i % j == 0:
                     break
             else:
-                s=s+i
+                s = s + i
     return s
+
+
 import ast
 
+
 def tolist(inp):
-    inp = "["+inp+"]"
+    inp = "[" + inp + "]"
     inp = ast.literal_eval(inp)
-    return (inp[0],inp[1])
+    return (inp[0], inp[1])
+
 
 def parse(inp):
     inp = ast.literal_eval(inp)
     return (inp)
 
+
 fncall = input()
 lparen = fncall.find("(")
 rparen = fncall.rfind(")")
 fname = fncall[:lparen]
-farg = fncall[lparen+1:rparen]
+farg = fncall[lparen + 1:rparen]
 
 if fname == "intreverse":
     arg = parse(farg)
@@ -60,58 +68,67 @@ elif fname == "sumprimes":
     print(sumprimes(arg))
 else:
     print("Function", fname, "unknown")
+
 
 def intreverse(n):
     ans = 0
     while n > 0:
-        (d,n) = (n%10,n//10)
-        ans = 10*ans + d
-    return(ans)
+        (d, n) = (n % 10, n // 10)
+        ans = 10 * ans + d
+    return (ans)
+
 
 def matched(s):
     nested = 0
-    for i in range(0,len(s)):
+    for i in range(0, len(s)):
         if s[i] == "(":
-            nested = nested+1
+            nested = nested + 1
         elif s[i] == ")":
-            nested = nested-1
+            nested = nested - 1
             if nested < 0:
-                return(False)
-    return(nested == 0)
+                return (False)
+    return (nested == 0)
+
 
 def factors(n):
     factorlist = []
-    for i in range(1,n+1):
-        if n%i == 0:
+    for i in range(1, n + 1):
+        if n % i == 0:
             factorlist = factorlist + [i]
-    return(factorlist)
+    return (factorlist)
+
 
 def isprime(n):
-    return(factors(n) == [1,n])
+    return (factors(n) == [1, n])
 
 
 def sumprimes(l):
     sum = 0
-    for i in range(0,len(l)):
+    for i in range(0, len(l)):
         if isprime(l[i]):
-            sum = sum+l[i]
-    return(sum)
+            sum = sum + l[i]
+    return (sum)
+
+
 import ast
 
+
 def tolist(inp):
-    inp = "["+inp+"]"
+    inp = "[" + inp + "]"
     inp = ast.literal_eval(inp)
-    return (inp[0],inp[1])
+    return (inp[0], inp[1])
+
 
 def parse(inp):
     inp = ast.literal_eval(inp)
     return (inp)
 
+
 fncall = input()
 lparen = fncall.find("(")
 rparen = fncall.rfind(")")
 fname = fncall[:lparen]
-farg = fncall[lparen+1:rparen]
+farg = fncall[lparen + 1:rparen]
 
 if fname == "intreverse":
     arg = parse(farg)
@@ -125,8 +142,9 @@ elif fname == "sumprimes":
 else:
     print("Function", fname, "unknown")
 
+
 def contracting(l):
-    differences = [abs(l[i] - l[i+1]) for i in range(len(l) - 1)]
+    differences = [abs(l[i] - l[i + 1]) for i in range(len(l) - 1)]
     for i in range(len(differences) - 1):
         if differences[i] <= differences[i + 1]:
             return False
@@ -151,17 +169,21 @@ def leftrotate(m):
         for j in range(n):
             rotated[n - 1 - j][i] = m[i][j]
     return rotated
+
+
 import ast
+
 
 def parse(inp):
     inp = ast.literal_eval(inp)
     return (inp)
 
+
 fncall = input()
 lparen = fncall.find("(")
 rparen = fncall.rfind(")")
 fname = fncall[:lparen]
-farg = fncall[lparen+1:rparen]
+farg = fncall[lparen + 1:rparen]
 
 if fname == "contracting":
     arg = parse(farg)
@@ -179,55 +201,60 @@ if fname == "leftrotate":
         print(ans)
     else:
         print("Side effect")
-#-------------------------------------------------------------------------------------------------------------------
+
+
+# -------------------------------------------------------------------------------------------------------------------
 def frequency(l):
-    dl1=set(l)
-    dl=list(dl1)
-    newl=[]
+    dl1 = set(l)
+    dl = list(dl1)
+    newl = []
     for i in dl:
         newl.append(l.count(i))
-    mi=min(newl)
-    ma=max(newl)
-    mil=[]
-    mal=[]
+    mi = min(newl)
+    ma = max(newl)
+    mil = []
+    mal = []
     for j in range(len(newl)):
-        if newl[j]==mi:
-
+        if newl[j] == mi:
             mil.append(dl[j])
-        if newl[j]==ma:
-
+        if newl[j] == ma:
             mal.append(dl[j])
     mil.sort()
     mal.sort()
-    return(mil,mal)
+    return (mil, mal)
+
 
 def onehop(l):
-    new=[]
+    new = []
     l.sort()
     for i in range(len(l)):
         for j in range(len(l)):
-            if l[i]!=l[j]:
-                if l[i][1]==l[j][0]:
-                    q=l[i][0]
-                    w=l[j][1]
-                    if q!=w:
-                        t=[q,w]
-                        t=tuple(t)
+            if l[i] != l[j]:
+                if l[i][1] == l[j][0]:
+                    q = l[i][0]
+                    w = l[j][1]
+                    if q != w:
+                        t = [q, w]
+                        t = tuple(t)
                         if t not in new:
                             new.append(tuple(t))
     new.sort()
     return (new)
+
+
 import ast
+
 
 def parse(inp):
     inp = ast.literal_eval(inp)
     return (inp)
 
+
 fncall = input()
 lparen = fncall.find("(")
 rparen = fncall.rfind(")")
 fname = fncall[:lparen]
-farg = fncall[lparen+1:rparen]
+farg = fncall[lparen + 1:rparen]
 
 if fname == "frequency":
     arg = parse(farg)
